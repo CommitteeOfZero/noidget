6fe473407e04ca6d9b323f5f2c07defb6872e563
Niklas K <drdaxxy@googlemail.com>
2018-03-23 15:12:50 +0100

    cross-thread exceptions

--- src/util/exception.h
+++ src/util/exception.h
@@ -1,12 +1,23 @@
 #pragma once
 
-#include <exception>
-#include <stdexcept>
+#include <QException>
 
-// Distinguish our exceptions from external code
-class NgException : public std::runtime_error {
+// For cross-thread exceptions, see http://doc.qt.io/qt-5/qexception.html
+class NgException : public QException {
    public:
-    explicit NgException(const std::string& what_arg)
-        : std::runtime_error(what_arg) {}
-    explicit NgException(const char* what_arg) : std::runtime_error(what_arg) {}
+    void raise() const { throw *this; }
+    NgException* clone() const { return new NgException(*this); }
+
+    NgException(const QString& what_arg) : QException(), _what(what_arg) {}
+    NgException(const std::string& what_arg) : QException() {
+        _what = QString::fromStdString(what_arg);
+    }
+    NgException(const char* what_arg) : QException() {
+        _what = QString::fromUtf8(what_arg);
+    }
+
+    const char* what() const noexcept { return _what.toUtf8().constData(); }
+
+   private:
+    QString _what;
 };
